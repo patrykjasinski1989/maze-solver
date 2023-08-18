73f9b0bb2e2902abe24dd3e610bb9597ea258eb1
Patryk <patrykjasinski1989@gmail.com>
2023-08-18 21:51:02 +0200

    Add Cell class and some docstrings

--- main.py
+++ main.py
@@ -1,24 +1,61 @@
+"""Maze generator and solver."""
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from tkinter import Tk, BOTH, Canvas
 
+DEFAULT_WIDTH = 2
+
+class Drawable(ABC):
+    @abstractmethod
+    def draw(self, canvas: Canvas) -> None:
+        """Draw the object on the given canvas."""
+        pass
 
 @dataclass
 class Point:
+    """Represents a 2D point with x and y coordinates."""
     x: int
     y: int
 
-
 @dataclass
-class Line:
+class Line(Drawable):
+    """Represents a line segment defined by two points, a color, and a width."""
     p1: Point
     p2: Point
+    color: str
+    width: int = DEFAULT_WIDTH
 
-    def draw(self, canvas: Canvas, fill_color: str, width: int) -> None:
-        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=width)
-        canvas.pack(fill=BOTH, expand=1)
+    def draw(self, canvas: Canvas) -> None:
+        """Draw the line on the given canvas."""
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=self.color, width=self.width)
 
+@dataclass
+class Cell(Drawable):
+    """Represents a cell in a maze."""
+    x1: int
+    y1: int
+    x2: int
+    y2: int
+    has_left_wall: bool = True
+    has_top_wall: bool = True
+    has_right_wall: bool = True
+    has_bottom_wall: bool = True
+    color: str = "black"
+    width: int = DEFAULT_WIDTH
+
+    def draw(self, canvas: Canvas) -> None:
+        """Draw the cell on the given canvas."""
+        if self.has_top_wall:
+            canvas.create_line(self.x1, self.y1, self.x2, self.y1, fill=self.color, width=self.width)
+        if self.has_left_wall:
+            canvas.create_line(self.x1, self.y1, self.x1, self.y2, fill=self.color, width=self.width)
+        if self.has_right_wall:
+            canvas.create_line(self.x2, self.y1, self.x2, self.y2, fill=self.color, width=self.width)
+        if self.has_bottom_wall:
+            canvas.create_line(self.x1, self.y2, self.x2, self.y2, fill=self.color, width=self.width)
 
 class Window:
+    """A window with a canvas to draw on."""
     def __init__(self, width: int, height: int) -> None:
         self.__root = Tk()
         self.__root.title("Maze Solver")
@@ -27,21 +64,25 @@ class Window:
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-    def redraw(self) -> None:
+    def _redraw(self) -> None:
+        """Update the window."""
         self.__root.update_idletasks()
         self.__root.update()
 
     def wait_for_close(self) -> None:
+        """Keep the window open until closed by the user."""
         self.__running = True
         while self.__running:
-            self.redraw()
-    
+            self._redraw()
+
     def close(self) -> None:
+        """Close the window."""
         self.__running = False
         self.__root.destroy()
 
-    def draw_line(self, line: Line, fill_color: str, width: int = 2) -> None:
-        line.draw(self.__canvas, fill_color, width)
+    def draw(self, drawable: Drawable) -> None:
+        """Draw the given drawable object on the canvas."""
+        drawable.draw(self.__canvas)
 
 
 if __name__ == "__main__":
@@ -52,14 +93,17 @@ if __name__ == "__main__":
 
     win = Window(WIDTH, HEIGHT)
 
-    lines_data = [
-        (Point(0, 0), Point(WIDTH, HEIGHT), "red"),
-        (Point(0, HEIGHT), Point(WIDTH, 0), "black"),
-        (Point(0, HALF_HEIGHT), Point(WIDTH, HALF_HEIGHT), "green"),
-        (Point(HALF_WIDTH, 0), Point(HALF_WIDTH, HEIGHT), "blue")
+    lines = [
+        Line(Point(0, 0), Point(WIDTH, HEIGHT), "red"),
+        Line(Point(0, HEIGHT), Point(WIDTH, 0), "black"),
+        Line(Point(0, HALF_HEIGHT), Point(WIDTH, HALF_HEIGHT), "green"),
+        Line(Point(HALF_WIDTH, 0), Point(HALF_WIDTH, HEIGHT), "blue")
     ]
 
-    for p1, p2, color in lines_data:
-        win.draw_line(Line(p1, p2), color)
+    for line in lines:
+        win.draw(line)
+
+    cell = Cell(HALF_WIDTH / 2, HALF_HEIGHT / 2, HALF_WIDTH * 3 / 2, HALF_HEIGHT * 3 / 2)
+    win.draw(cell)
 
     win.wait_for_close()
